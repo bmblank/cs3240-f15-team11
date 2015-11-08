bb2aae28d07be8bf467b90678a736df54e8554ac
Jennifer <jrh7qp@virginia.edu>
2015-11-08 15:21:57 -0500

    Attempting messaging system

--- projectSite/urls.py
+++ projectSite/urls.py
@@ -19,4 +19,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    (r'^messages/', include('postman.urls', namespace='postman', app_name='postman')),
 ]
