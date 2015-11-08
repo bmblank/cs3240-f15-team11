a866b7d2902987df9c4f9fc9b18d9b8bdc57d6f3
Michelle Yao <xy4kj@virginia.edu>
2015-11-08 16:05:18 -0500

    postman fix

--- projectSite/settings.py
+++ projectSite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/dev/ref/settings/
 """
 
 import os
+import postman
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -56,7 +57,7 @@ ROOT_URLCONF = 'projectSite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'), ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- projectSite/urls.py
+++ projectSite/urls.py
@@ -14,10 +14,13 @@ Including another URLconf
     2. Import the include() function: from django.conf.urls import url, include
     3. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
+
+import postman
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    (r'^messages/', include('postman.urls', namespace='postman', app_name='postman')),
+    url(r'^messages/', include('postman.urls', namespace='postman', app_name='postman')),
+    # (r'^messages/', include('postman.urls', namespace='postman', app_name='postman')),
 ]
