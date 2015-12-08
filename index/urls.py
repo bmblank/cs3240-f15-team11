dcdc8551ac17c026601e53227dfe5f8c2f4dd914
Ben Browning <brb7nu@virginia.edu>
2015-12-07 23:07:06 -0500

    add redirect to login page

--- index/urls.py
+++ index/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(r'^$', views.Login),
     url(r'^login/$', views.Login),
     url(r'^logout/$', views.Logout),
     url(r'^home/$', views.Home),
