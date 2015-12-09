8de7d0108c44a7244668d7f15082779b297eaea9
Michelle Yao <xy4kj@virginia.edu>
2015-12-09 03:50:01 -0500

    heroku postgresql stuff

--- memos/migrations/0001_initial.py
+++ memos/migrations/0001_initial.py
@@ -2,22 +2,37 @@
 from __future__ import unicode_literals
 
 from django.db import migrations, models
+from django.conf import settings
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
+        ('auth', '0006_require_contenttypes_0002'),
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
     operations = [
         migrations.CreateModel(
+            name='Key',
+            fields=[
+                ('user', models.OneToOneField(to=settings.AUTH_USER_MODEL, primary_key=True, serialize=False)),
+                ('privateKey', models.TextField(blank=True, null=True)),
+                ('publicKey', models.TextField(blank=True, null=True)),
+                ('rKey', models.TextField(blank=True, null=True)),
+            ],
+        ),
+        migrations.CreateModel(
             name='Memo',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True, serialize=False)),
-                ('sender', models.CharField(max_length=200)),
-                ('recipient', models.CharField(max_length=200)),
-                ('subject', models.CharField(max_length=100)),
+                ('id', models.AutoField(auto_created=True, serialize=False, verbose_name='ID', primary_key=True)),
+                ('recipient_username', models.CharField(max_length=100)),
+                ('subject', models.CharField(max_length=100, default=None)),
                 ('body', models.TextField(max_length=1000)),
+                ('created', models.DateTimeField(default=None)),
+                ('encrypted', models.BooleanField(default=False)),
+                ('recipient', models.ForeignKey(to=settings.AUTH_USER_MODEL, related_name='recipient', null=True)),
+                ('sender', models.ForeignKey(to=settings.AUTH_USER_MODEL, related_name='sender', null=True)),
             ],
         ),
     ]
